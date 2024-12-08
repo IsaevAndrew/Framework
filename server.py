ea0ff6f00c7556b3c5560e7ffe37510d7f4616e1
dr0nchikk <i.4ndrew1@yandex.ru>
2024-12-08 12:57:30 +0300

    change wsgi to threading and socket

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 import mimetypes
-from wsgiref.simple_server import make_server
-from router import Routes
+import threading
+import socket
+from router import Router
 import os
 
 
 class SimpleFramework:
     def __init__(self, static_folder="static", template_folder="templates"):
-        self.routes = Routes()
+        self.routes = Router()
         self.static_folder = static_folder
         self.template_folder = template_folder
 
@@ -91,7 +92,29 @@ class SimpleFramework:
         return [response_body.encode("utf-8")]
 
     def start_server(self, host="127.0.0.1", port=8080):
-        """ Запуск WSGI-сервера """
-        with make_server(host, port, self) as httpd:
-            print(f"Server started at http://{host}:{port}")
-            httpd.serve_forever()
+        """ Запуск сервера с использованием многопоточности """
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        server_socket.bind((host, port))
+        server_socket.listen(5)
+
+        print(f"Server started at http://{host}:{port}")
+
+        def handle_client(client_socket):
+            try:
+                request = client_socket.recv(1024).decode()
+                if request:
+                    response = self.handle_request(request)
+                    client_socket.sendall(response.encode())
+            except Exception as e:
+                print(f"Error handling client: {e}")
+            finally:
+                client_socket.close()
+
+        while True:
+            client_socket, client_address = server_socket.accept()
+            print(f"Connection established with {client_address}")
+            client_thread = threading.Thread(target=handle_client,
+                                             args=(client_socket,))
+            client_thread.daemon = True
+            client_thread.start()
